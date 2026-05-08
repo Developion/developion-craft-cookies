91a9f2fc8f8a68d9f0512199b011fd1f01e56e5b
dusan-stojnic <111434032+dusan-stojnic@users.noreply.github.com>
2026-05-08 14:28:53 +0200

    added banner position setting (#18)
    * added banner position setting
    
    * added styles for horizontal (center) position
    
    ---------
    
    Co-authored-by: Marko Glumac <marko.glumac@developion.com>

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -14,6 +14,7 @@ class Settings extends Model
 
 	public string $cookieNamePrefix = 'cookieConsent_';
 	public string $cookieManagerUrl = '$COOKIE_MANAGER_URL';
+	public string $cookieBannerPosition = 'bottom--center';
 	public string $apiKey = '';
 
 	public function rules(): array
